00435a970e53b9a59805e91ff7de7ae699d9217c
JAFET MARTINEZ MEZA <91506409+mmjafet@users.noreply.github.com>
2025-01-07 10:35:24 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
 from flask import Flask, request, jsonify, render_template
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
